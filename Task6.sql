f2184c91600f96e9073acc223a415f16c271ffdb
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-08-25 02:58:36 +0300

    added.

---
+++ Task6.sql
@@ -0,0 +1,25 @@
+-- 1. Task: film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+
+SELECT ROUND(AVG(rental_rate), 2) FROM film;
+
+
+
+-- 2. Task: film tablosunda bulunan filmlerden kaç tanesi 'C' karekteri ile başlar?
+
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'C%';
+
+
+
+-- 3. Task: film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+
+SELECT MAX(length) FROM film
+WHERE rental_rate = 0.99;
+
+
+
+-- 4. Task: film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 150;
+
